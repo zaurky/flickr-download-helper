4321ed9ecf4bbbd68bec1ebc747d7a349feeca04
zaurky <zaurky@zeb.re>
2011-03-02 10:46:54 +0100

    sort contacts_rev output to avoid having diff when there is none

--- bin/contactRevChange.sh
+++ bin/contactRevChange.sh
@@ -3,8 +3,8 @@
 . /etc/fdh/fdh.sh
 FDHPATH=$FLICKRDIR
 
-mv -f $FDHPATH/files/contacts_rev.new $FDHPATH/files/contacts_rev.old
-$FDHPATH/bin/getContacts.py --gcf nsid,revcontact,revfriend,revfamily --acf > $FDHPATH/files/contacts_rev.new
+cat $FDHPATH/files/contacts_rev.new | sort > $FDHPATH/files/contacts_rev.old
+$FDHPATH/bin/getContacts.py --gcf nsid,revcontact,revfriend,revfamily --acf | sort > $FDHPATH/files/contacts_rev.new
 
 diff $FDHPATH/files/contacts_rev.old $FDHPATH/files/contacts_rev.new | grep ' ' > $FDHPATH/files/contacts_rev.diff
 if [ `wc -l $FDHPATH/files/contacts_rev.diff | awk '{print $1}'` -ne 0 ]; then
