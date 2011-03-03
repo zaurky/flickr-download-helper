668526caa9777a44713de84649af040b6bc507ea
zaurky <zaurky@zeb.re>
2011-03-03 18:01:42 +0100

    new script to follow user change by username

---
+++ bin/contactRevChangeName.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. /etc/fdh/fdh.sh
+FDHPATH=$FLICKRDIR
+
+cat $FDHPATH/files/contacts_rev_name.new | sort > $FDHPATH/files/contacts_rev_name.old
+$FDHPATH/bin/getContacts.py --gcf nsid,username --acf | sort > $FDHPATH/files/contacts_rev_name.new
+
+diff $FDHPATH/files/contacts_rev_name.old $FDHPATH/files/contacts_rev_name.new | grep ' ' > $FDHPATH/files/contacts_rev_name.diff
+if [ `wc -l $FDHPATH/files/contacts_rev_name.diff | awk '{print $1}'` -ne 0 ]; then
+    if [ -s $FDHPATH/files/contacts_rev_name.diff ]; then
+        date >> $FDHPATH/files/contacts_rev_change_name
+        cat $FDHPATH/files/contacts_rev_name.diff >> $FDHPATH/files/contacts_rev_change_name
+    fi
+fi
+rm -f $FDHPATH/files/contacts_rev_name.diff $FDHPATH/files/contacts_rev_name.old
+
