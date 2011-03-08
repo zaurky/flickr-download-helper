fe4f0976fa8a32fa1fd55d82457517d95fd2a1ab
zaurky <zaurky@zeb.re>
2011-03-08 11:03:42 +0100

    also search in previous log files

--- bin/getProcessLogs.sh
+++ bin/getProcessLogs.sh
@@ -3,7 +3,12 @@
 . /etc/fdh/fdh.sh
 
 PROCESS_ID=$1
+if [ "x$2" == "x" ]; then
+    DATE=""
+else
+    DATE=".$2.0"
+fi
 
-ID=`grep "running as $PROCESS_ID" "$FLICKRDIR/log/fdh.log" | awk '{print $4}' | sed -e 's/://'`
-grep $ID "$FLICKRDIR/log/fdh.log"
+ID=`grep "running as $PROCESS_ID" "$FLICKRDIR/log/fdh.log$DATE" | awk '{print $4}' | sed -e 's/://'`
+grep $ID "$FLICKRDIR/log/fdh.log$DATE"
 
