1f7941737cca10f29ef51f224e79b9e7aa0c7137
zaurky <zaurky@zeb.re>
2011-04-08 10:51:49 +0200

    new cron to get from urls every day

--- conf/fdh.sh
+++ conf/fdh.sh
@@ -3,9 +3,11 @@ FLICKRPHOTODIR="/mnt/flickr/photo/"
 
 NEWS_DIR="/mnt/flickr/news/daily"
 THUMBDIR="/mnt/flickr/thumb"
+CACHEDIR="/usr/share/fdh"
+FILESDIR="$FLICKRDIR/files"
 
 TMPDIR='/tmp'
 
 MAIL=""
-
+MAILCACHE="$CACHEDIR/mails"
 
