2552c4b9ee42819c5cf6915866eda5893dedbd6c
zaurky <zaurky@zeb.re>
2011-06-01 14:07:04 +0200

    dont use ## anymore

--- bin/create_news_thumbnail.sh
+++ bin/create_news_thumbnail.sh
@@ -13,8 +13,8 @@ fi
 find $WORKING_DIR -type d > $TMPDIR/fdh.thumbgen.dirs.$TAG
 
 rm -f "$TMPDIR/fdh.thumbgen.files.$TAG"
-for i in `echo $WORKING_DIR/**/*(oc@) | sed -e 's| |##|g' | sed -e 's|##/| /|g'`; do
-    i=`echo "$i" | sed -e 's|##| |g'`
+for i in `echo $WORKING_DIR/**/*(oc@) | sed -e 's| |~~|g' | sed -e 's|~~/| /|g'`; do
+    i=`echo "$i" | sed -e 's|~~| |g'`
     echo "$i" >> $TMPDIR/fdh.thumbgen.files.$TAG
 done
 
@@ -27,25 +27,26 @@ for i in `cat $TMPDIR/fdh.thumbgen.dirstocreate.$TAG`; do mkdir -p "$i"; done
 rm -f $TMPDIR/fdh.thumbgen.dirstocreate.$TAG
 
 if [ "x$DAY" != "x" ]; then
-    rm -f "$THUMBDIR/$DAY.files"
+    rm -f "$THUMBDIR/$DAY.files.tmp"
 else
     rm -f "$THUMBDIR/*.files"
 fi
 
-for i in `cat $TMPDIR/fdh.thumbgen.files.$TAG | sed -e 's| |##|g'`; do
-    THUMBFILE=`echo "$i" |  sed -e "s|$NEWS_DIR|$THUMBDIR\/square|" | sed -e 's|##| |g'`
-    MOBILEFILE=`echo "$i" |  sed -e "s|$NEWS_DIR|$THUMBDIR\/mobile|" | sed -e 's|##| |g'`
+for i in `cat $TMPDIR/fdh.thumbgen.files.$TAG | sed -e 's| |~~|g'`; do
+    THUMBFILE=`echo "$i" |  sed -e "s|$NEWS_DIR|$THUMBDIR\/square|" | sed -e 's|~~| |g'`
+    MOBILEFILE=`echo "$i" |  sed -e "s|$NEWS_DIR|$THUMBDIR\/mobile|" | sed -e 's|~~| |g'`
 
     DATE=`dirname "$FILE"`
     DATE=`basename "$DATE"`
-    FILE=`echo "$i" | sed -e 's|##| |g'`
+    FILE=`echo "$i" | sed -e 's|~~| |g'`
     if [ ! -e "$THUMBFILE" ]; then
         convert -thumbnail "80x80" "$FILE" "$THUMBFILE"
     fi
     if [ ! -e "$MOBILEFILE" ]; then
         convert -resize "800x800" "$FILE" "$MOBILEFILE"
     fi
-    echo `basename "$FILE"` >> "$THUMBDIR/$DATE.files"
+    echo `basename "$FILE"` >> "$THUMBDIR/$DATE.files.tmp"
 done
+mv "$THUMBDIR/$DATE.files.tmp" "$THUMBDIR/$DATE.files"
 
-rm -f $TMPDIR/fdh.thumbgen.dirs.$TAG $TMPDIR/fdh.thumbgen.dirs.$TAG
+rm -f $TMPDIR/fdh.thumbgen.dirs.$TAG $TMPDIR/fdh.thumbgen.files.$TAG
