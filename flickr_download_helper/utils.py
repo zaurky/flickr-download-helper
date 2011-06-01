41ab820025dec68f10f0d52894fc06b2025df2b3
zaurky <zaurky@zeb.re>
2011-06-01 14:05:07 +0200

    add get_video function

--- flickr_download_helper/utils.py
+++ flickr_download_helper/utils.py
@@ -1,6 +1,8 @@
 from flickr_download_helper.config import OPT
 from flickr_download_helper.logger import Logger
 import sys
+import re
+import os
 
 #################### UTILITIES
 def waitFor(message, *attr):
@@ -32,4 +34,15 @@ def extends(orig, ext):
             orig[k] = ext[k]
     return orig
 
+
+def get_video(file):
+    if not os.path.exists(file):
+        return False
+    file = open(file, 'rb')
+    for line in file.readlines():
+        m = re.search('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*?Get the video (http://www.flickr.com/photos/[^\/]+/\d+/)', line)
+        if m is not None:
+            print m.group(2)
+    file.close()
+    return True
 
