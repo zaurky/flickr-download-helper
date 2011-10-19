20d408faa5c1aef2f7150a836687776e2f503e17
zaurky <zaurky@zeb.re>
2011-10-19 14:04:17 +0200

    correct variable use

--- bin/contactRevChangeName.sh
+++ bin/contactRevChangeName.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 . /etc/fdh/fdh.sh
-FDHPATH=$FLICKRDIR
 
-cat $FDHPATH/files/contacts_rev_name.new | sort > $FDHPATH/files/contacts_rev_name.old
-$FDHPATH/bin/getContacts.py --gcf nsid,username | sort > $FDHPATH/files/contacts_rev_name.new
+cat $FLICKRDIR/files/contacts_rev_name.new | sort > $FLICKRDIR/files/contacts_rev_name.old
+$FLICKRBINDIR/getContacts.py --gcf nsid,username | sort > $FLICKRDIR/files/contacts_rev_name.new
 
-diff $FDHPATH/files/contacts_rev_name.old $FDHPATH/files/contacts_rev_name.new | grep ' ' > $FDHPATH/files/contacts_rev_name.diff
-if [ `wc -l $FDHPATH/files/contacts_rev_name.diff | awk '{print $1}'` -ne 0 ]; then
-    if [ -s $FDHPATH/files/contacts_rev_name.diff ]; then
-        date >> $FDHPATH/files/contacts_rev_change_name
-        cat $FDHPATH/files/contacts_rev_name.diff >> $FDHPATH/files/contacts_rev_change_name
+diff $FLICKRDIR/files/contacts_rev_name.old $FLICKRDIR/files/contacts_rev_name.new | grep ' ' > $FLICKRDIR/files/contacts_rev_name.diff
+if [ `wc -l $FLICKRDIR/files/contacts_rev_name.diff | awk '{print $1}'` -ne 0 ]; then
+    if [ -s $FLICKRDIR/files/contacts_rev_name.diff ]; then
+        date >> $FLICKRDIR/files/contacts_rev_change_name
+        cat $FLICKRDIR/files/contacts_rev_name.diff >> $FLICKRDIR/files/contacts_rev_change_name
     fi
 fi
-rm -f $FDHPATH/files/contacts_rev_name.diff $FDHPATH/files/contacts_rev_name.old
+rm -f $FLICKRDIR/files/contacts_rev_name.diff $FLICKRDIR/files/contacts_rev_name.old
 
