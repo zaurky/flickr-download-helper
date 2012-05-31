38bc678db149d6319cb806ecf8902b3f2e7aa4b4
zaurky <zaurky@zeb.re>
2012-05-31 16:17:04 +0200

    filter lines to avoid bad noise

--- bin/contactRevChangeName.sh
+++ bin/contactRevChangeName.sh
@@ -2,8 +2,8 @@
 
 . /etc/fdh/fdh.sh
 
-cat $FLICKRDIR/files/contacts_rev_name.new | sort > $FLICKRDIR/files/contacts_rev_name.old
-$FLICKRBINDIR/getContacts.py --gcf nsid,username | sort > $FLICKRDIR/files/contacts_rev_name.new
+cat $FLICKRDIR/files/contacts_rev_name.new | grep -v 'logging as' | sort > $FLICKRDIR/files/contacts_rev_name.old
+$FLICKRBINDIR/getContacts.py --gcf nsid,username | grep -v 'logging as' | sort > $FLICKRDIR/files/contacts_rev_name.new
 
 diff $FLICKRDIR/files/contacts_rev_name.old $FLICKRDIR/files/contacts_rev_name.new | grep ' ' > $FLICKRDIR/files/contacts_rev_name.diff
 if [ `wc -l $FLICKRDIR/files/contacts_rev_name.diff | awk '{print $1}'` -ne 0 ]; then
