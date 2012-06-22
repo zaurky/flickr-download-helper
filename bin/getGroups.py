6cdc5d6de1743c35c64b36be02aada8f58204de8
zaurky <zaurky@zeb.re>
2012-06-22 16:30:01 +0200

    dont get group file if it already exists

--- bin/getGroups.py
+++ bin/getGroups.py
@@ -6,13 +6,21 @@ from flickr_download_helper.logger import Logger
 import flickr_download_helper
 from flickr_download_helper.api import groupFromScratch, getUserGroups
 
+import os
+
 api, token = flickr_download_helper.main_init(False)
 
 INS['put_group_in_session'] = True
 groups = getUserGroups(api, token, OPT.my_id, page = 1)
 i = 0
 for group in groups:
-    if group['nsid'] in OPT.skiped_group:
+    group_id = group['nsid']
+    if group_id in OPT.skiped_group:
+        continue
+    if os.path.exists(os.path.join(OPT.groups_full_content_dir, group_id)):
+        continue
+    if os.path.exists(os.path.join(OPT.groups_full_content_dir,
+            "%s_0" % group_id)):
         continue
     Logger().warn("scan_group %d/%d"%(i, len(groups)))
-    groupFromScratch(api, token, group['nsid'])
+    groupFromScratch(api, token, group_id)
