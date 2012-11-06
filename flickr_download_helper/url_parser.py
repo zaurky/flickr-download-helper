74f60642c712ddfeedeb0e64ff70c3ae99895cbe
zaurky <zaurky@zeb.re>
2012-11-06 16:24:07 +0100

    clean url parser

--- flickr_download_helper/url_parser.py
+++ flickr_download_helper/url_parser.py
@@ -1,61 +1,73 @@
-
 from flickr_download_helper.types import FDHPR
+from flickr_download_helper.logger import Logger
+
 
 class UrlParser:
-    def  __init__(self, url = None):
+
+    def  __init__(self, url=None):
         self.url = url
+
     def parse(self, url=None):
-        if url == None:
-            if self.url == None: return (FDHPR.ERROR_NOURL)
+        if not url:
+            if not self.url:
+                return (FDHPR.ERROR_NOURL)
             url = self.url
+
         a_url = url.split('/')
-        if a_url[len(a_url) - 1] == '':
+        if not a_url[-1]:
             a_url.pop()
+
         if a_url[2] != 'www.flickr.com':
-            print "url is not a flickr url! %s"%url
+            Logger().warn("url is not a flickr url! %s" % url)
             return (FDHPR.ERROR_NOTFLICKR)
+
         if a_url[len(a_url) - 2] == 'with' or a_url[len(a_url) - 2] == 'in':
             a_url.pop()
             a_url.pop()
-        print a_url
-        print len(a_url)
+
+        keyword, param, extra1, extra2 = a_url[3:7]
+
+        Logger().debug("%s %s %s %s" % (keyword, param, extra1, extra2))
 
         if len(a_url) == 5:
-            if a_url[3] == 'photos':
+            if keyword == 'photos':
                 # should be a full flickr account
-                print "account %s"%a_url[4]
-                return (FDHPR.USER, a_url[4])
-            elif a_url[3] == 'people':
+                Logger().info("account %s" % param)
+                return (FDHPR.USER, param)
+            elif keyword == 'people':
                 # should be a user's profile
-                print "profile of user %s"%a_url[4]
-                return (FDHPR.PROFILE, a_url[4])
-            elif a_url[3] == 'groups':
-                print "group %s"%a_url[4]
-                return (FDHPR.GROUP, a_url[4])
+                Logger().info("profile of user %s" % param)
+                return (FDHPR.PROFILE, param)
+            elif keyword == 'groups':
+                Logger().info("group %s" % param)
+                return (FDHPR.GROUP, param)
+
         elif len(a_url) == 6:
-            if a_url[3] == 'photos' and a_url[5] == 'sets':
-                print "photosets in account %s"%( a_url[4])
-                return (FDHPR.PHOTOSETS, a_url[4])
-            elif a_url[3] == 'photos':
+            if keyword == 'photos' and extra1 == 'sets':
+                Logger().info("photosets in account %s" % (param))
+                return (FDHPR.PHOTOSETS, param)
+            elif keyword == 'photos':
                 # should be a picture in a flickr account
-                print "photo %s in account %s"%(a_url[5], a_url[4])
-                return (FDHPR.PHOTO, a_url[4], a_url[5])
-            elif a_url[3] == 'groups':
-                print "group %s"%a_url[4]
-                return (FDHPR.GROUP, a_url[4])
+                Logger().info("photo %s in account %s" % (extra1, param))
+                return (FDHPR.PHOTO, param, extra1)
+            elif keyword == 'groups':
+                Logger().info("group %s" % param)
+                return (FDHPR.GROUP, param)
+
         elif len(a_url) == 7:
-            if a_url[3] == 'photos' and a_url[5] == 'collections':
+            if keyword == 'photos' and extra1 == 'collections':
                 # should be a collection in a flick account
-                print "collection %s in account %s"%(a_url[6], a_url[4])
-                return (FDHPR.COLLECTION, a_url[4], a_url[6])
-            elif a_url[3] == 'photos' and a_url[5] == 'sets':
+                Logger().info("collection %s in account %s" % (extra2, param))
+                return (FDHPR.COLLECTION, param, extra2)
+            elif keyword == 'photos' and extra1 == 'sets':
                 # should be a set
-                print "set %s %s"%(a_url[4], a_url[6])
-                return (FDHPR.SET, a_url[4], a_url[6])
-            elif a_url[3] == 'photos' and a_url[5] == 'tags':
-                print "tag %s %s"%(a_url[4], a_url[6])
-                return (FDHPR.TAG, a_url[4], a_url[6])
-            elif a_url[3] == 'groups' and a_url[5] == 'pool':
-                print "user %s in groups %s"%(a_url[6], a_url[4])
-                return (FDHPR.INGROUP, a_url[6], a_url[4])
+                Logger().info("set %s %s" % (param, extra2))
+                return (FDHPR.SET, param, extra2)
+            elif keyword == 'photos' and extra1 == 'tags':
+                Logger().info("tag %s %s" % (param, extra2))
+                return (FDHPR.TAG, param, extra2)
+            elif keyword == 'groups' and extra1 == 'pool':
+                Logger().info("user %s in groups %s" % (extra2, param))
+                return (FDHPR.INGROUP, extra2, param)
+
         return (FDHPR.ERROR)
