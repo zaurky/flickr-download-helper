7dfbdc42a3c3fc8a44679a8347b61b1f14d28938
zaurky <zaurky@zeb.re>
2012-11-06 16:29:49 +0100

    clean code

--- flickr_download_helper/utils.py
+++ flickr_download_helper/utils.py
@@ -12,40 +12,42 @@ def waitFor(message, *attr):
 
     try:
         Logger().info(message % attr)
-    except TypeError, e:
+    except TypeError:
         info = sys.exc_info()
-        Logger().error("this is a development bug, please report this traceback")
+        Logger().error(
+            "this is a development bug, please report this traceback")
         Logger().print_tb(info[2])
         return False
 
     Logger().prompt("do you want to try again [y/n]")
-    line = sys.stdin.readline()
-    line = line.rstrip()
-    if line.lower() == 'y' or line.lower() == 'yes':
-        return True
-    return False
+    return sys.stdin.readline().rstrip().lower() in ('y', 'yes')
 
 
 def extends(orig, ext):
-    if type(orig) != dict or type(ext) != dict:
+    if not (isinstance(orig, dict) and isinstance(ext, dict)):
         raise Exception('extends only work with 2 dict!')
+
     for k in ext:
         if k in orig and orig[k] != ext[k]:
-            Logger().warn("key %s is in the two dict but don't have the same value" % k)
+            Logger().warn(
+                "key %s is in the two dict but don't have the same value" % k)
         else:
             orig[k] = ext[k]
     return orig
 
 
-def get_video(file):
-    if not os.path.exists(file):
+def get_video(filename):
+    if not os.path.exists(filename):
         return False
-    file = open(file, 'rb')
-    for line in file.readlines():
-        m = re.search('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*?Get the video (http://www.flickr.com/photos/[^\/]+/\d+/)', line)
-        if m is not None:
+
+    f = open(filename, 'rb')
+    for line in f.readlines():
+        m = re.search('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*?" \
+            "Get the video (http://www.flickr.com/photos/[^\/]+/\d+/)', line)
+        if m:
             print m.group(2)
-    file.close()
+
+    f.close()
     return True
 
 
@@ -57,4 +59,3 @@ def readFile(filename):
         return ret
     else:
         Logger().error("file not found %s" % filename)
-    return None
