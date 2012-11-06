5aefb234540b115f953e207d98fccebf85fae487
zaurky <zaurky@zeb.re>
2012-11-06 14:39:00 +0100

    clean proxy code

--- flickr_download_helper/proxy.py
+++ flickr_download_helper/proxy.py
@@ -1,6 +1,7 @@
 import urllib2
 from flickr_download_helper.logger import Logger
 
+
 class FDHProxySettings():
     def setValues(self, opt):
         self.user = opt.proxy_user
@@ -9,25 +10,30 @@ class FDHProxySettings():
         self.port = opt.proxy_port
         self.need_activation = opt.proxy
 
+    def _init_handler(self, url):
+        Logger().debug(url)
+        return urllib2.ProxyHandler({"http": url})
+
     def activate(self):
         if self.need_activation:
             Logger().info("\n== Initialising the proxy")
-            Logger().debug("%s:%s@%s:%i"%(self.user, self.password, self.host,self.port))
 
             proxy_info = {
-                    'user':self.user,
-                    'pass':self.password,
-                    'host':self.host,
-                    'port':self.port
+                    'user': self.user,
+                    'pass': self.password,
+                    'host': self.host,
+                    'port': self.port,
             }
+
             if self.user:
                 if self.password:
-                    proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)d" % proxy_info})
+                    url = "http://%(user)s:%(pass)s@%(host)s:%(port)d" % proxy_info
                 else:
-                    proxy_support = urllib2.ProxyHandler({"http" : "http://%(user)s@%(host)s:%(port)d" % proxy_info})
+                    url = "http://%(user)s@%(host)s:%(port)d" % proxy_info
             else:
-                proxy_support = urllib2.ProxyHandler({"http" : "http://%(host)s:%(port)d" % proxy_info})
+                url = "http://%(host)s:%(port)d" % proxy_info
 
+            proxy_support = self._init_handler(url)
             opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
 
             # install it
