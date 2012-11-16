cb689e3c855f8c98e682d7088331f8c0560b39a8
zaurky <zaurky@zeb.re>
2012-11-16 16:49:18 +0100

    other way to do a singleton

--- flickr_download_helper/utils.py
+++ flickr_download_helper/utils.py
@@ -11,6 +11,15 @@ INV_SIZES.reverse()
 
 
 #################### UTILITIES
+def singleton(cls):
+    instances = {}
+    def getinstance(*args, **kargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kargs)
+        return instances[cls]
+    return getinstance
+
+
 def waitFor(message, *attr):
     if not OPT.interactive:
         return False
