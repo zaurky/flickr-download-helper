8b63a7f1e7448ab60212e7755fecdbc391f46e78
zaurky <zaurky@zeb.re>
2012-11-16 12:51:21 +0100

    add docstrings

--- flickr_download_helper/api_utils.py
+++ flickr_download_helper/api_utils.py
@@ -1,13 +1,19 @@
+"""
+Module that expose 2 methods to reflect flickr api
+"""
+
 from flickr_download_helper.api import json_request
 
 
 def method_info(api, token, method_name):
+    """ get information on a given flick api method """
     rsp_json = json_request(api, token, 'flickr.reflection.getMethodInfo',
         "method info for %s", [method_name], method_name=method_name)
     return rsp_json if rsp_json else None
 
 
 def reflect(api, token):
+    """ get the list of flickr api methods """
     rsp_json = json_request(api, token, 'flickr.reflection.getMethods',
         "methods")
     return rsp_json['methods']['method'] if rsp_json else None
