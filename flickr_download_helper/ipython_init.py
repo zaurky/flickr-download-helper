4fea20e85fc3e982f63560da2acc8aac540d2a4c
zaurky <zaurky@zeb.re>
2012-11-21 12:07:22 +0100

    adapt ipython init to new API calls

--- flickr_download_helper/ipython_init.py
+++ flickr_download_helper/ipython_init.py
@@ -1,21 +1,8 @@
 from flickr_download_helper.config import OPT, OptConfigReader
 from flickr_download_helper.proxy import FDHProxySettings
 from flickr_download_helper.logger import Logger
-from flickr_download_helper.api import initialisationFlickrApi
 from flickr_download_helper.api import *
 import flickr_download_helper.api
-
-config = OptConfigReader()
-config.setup()
-
-Logger().setup()
-Logger().warn("####################################################################")
-
-proxy = FDHProxySettings()
-proxy.setValues(OPT)
-proxy.activate()
-
-api, token = initialisationFlickrApi(OPT)
 
 print "\t" + "\n\t".join(filter(lambda func:
     'get' in func or 'search' in func,
@@ -26,11 +13,17 @@ print "\t" + "\n\t".join(filter(lambda func:
 print "#"*80
 print """
 contact_name = ''
-contact = getUserFromAll(api, contact_name)
-photos = getUserPhotos(api, token, contact['id'])
+contact = api.getUserFromAll(contact_name)
+photos = api.getUserPhotos(contact['id'])
+
 other = filter(lambda x: x['media'] != 'photo', photos)
-v = other[0]
-info = getPhotoInfo(api, token, v['id'])
-size = getPhotoSize(api, token, v['id'])
+
+photo = photos[0]
+info = api.getPhotoInfo(photo['id'])
+size = api.getPhotoSize(photo['id'])
 """
 print "#"*80
+
+api = API(False)
+
+me = OPT.my_id
