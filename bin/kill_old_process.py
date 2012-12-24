d340782f37d719db72e347c264533ea6e624cb16
zaurky <zaurky@zeb.re>
2012-12-24 11:32:28 +0100

    make kill old process work (much simpler)

--- bin/kill_old_process.py
+++ bin/kill_old_process.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python
 
-import shlex, subprocess
-command_line = 'ps -eo pid,start_time,cmd' # | grep py | grep -v grep'
-args = shlex.split(command_line)
-proc = subprocess.Popen(args,stdout=subprocess.PIPE)
-
+import os
+import signal
 from datetime import datetime, timedelta
-def parse_date(date):
-    if date.find(':') < 0: # only the date
-        return datetime.strptime("%s%s" % (datetime.now().year, date), '%Y%b%d')
-    today = datetime.now().strftime("%Y%m%d")
-    return datetime.strptime("%s %s" % (today, date), '%Y%m%d %H:%M')
 
-for line in proc.stdout.readlines():
-    if line.find('py') < 0:
-        continue
 
-    line = line.strip()
+command_line = 'ps -eo pid,lstart,cmd | grep py | grep -v grep'
+fd = os.popen(command_line)
+
+
+for line in [line.strip() for line in fd.readlines()]:
     a_line = line.split(' ')
     a_line.reverse()
 
     pid = a_line.pop()
-    date = parse_date(a_line.pop())
-    a_line.reverse()
-    line = ' '.join(a_line)
 
-    delta = date - datetime.now()
+    date = a_line[-5:-1]
+    date = dict(zip(('year', 'time', 'day', 'month'), date))
+    date = "%(year)s-%(month)s-%(day)s %(time)s" % date
+    start = datetime.strptime(date, '%Y-%b-%d %H:%M:%S')
+    delta = datetime.now() - start
 
-    if delta < timedelta(hours=-2):
-        print line
+    if delta.days >= 2:
+        print "killing %s" % pid
+        os.kill(pid, signal.SIGKILL)
+        continue
 
+    if delta.seconds > 7200:
+        print "%s running for %ss" % (pid, delta.seconds)
