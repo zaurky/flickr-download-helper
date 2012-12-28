2c1ff0ac167ed4902ad995b4b7b8983adedb3c18
zaurky <zaurky@zeb.re>
2012-12-28 15:59:00 +0100

    simplify code

--- bin/old_contacts.py
+++ bin/old_contacts.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""
+Generate the cache of old contacts to be able to only look at active contacts.
+
+"""
+
 
 import pickle
 from datetime import datetime, timedelta
@@ -7,57 +12,31 @@ from flickr_download_helper.api import API
 
 api = API(False)
 contacts = api.getContactList()
-
-to_remove_30 = []
-to_remove_60 = []
-to_remove_90 = []
-to_remove_180 = []
-
 now = datetime.now()
+
+to_remove = {}
 deltas = {}
+
+
 for delta in (30, 60, 90, 180):
     deltas[delta] = (now - timedelta(days=delta)).strftime('%s')
 
 
 def has_photos(nsid, delta):
-    return len(api.getUserPhotos(nsid, deltas[delta], limit=2)) != 0
+    """check that this contact has some photos since delta"""
+    return len(api.getUserPhotos(nsid, delta, limit=2)) != 0
 
 
-for c in contacts:
-    if has_photos(c['nsid'], 180):
-        if has_photos(c['nsid'], 90):
-            if has_photos(c['nsid'], 60):
-                if has_photos(c['nsid'], 30):
-                    print "keep : %s" % (c['nsid'])
-                else:
-                   print "30 should remove : %s" % (c['nsid'])
-                   to_remove_30.append(c['nsid'])
-            else:
-                print "60 should remove : %s" % (c['nsid'])
-                to_remove_60.append(c['nsid'])
-        else:
-            print "90 should remove : %s" % (c['nsid'])
-            to_remove_90.append(c['nsid'])
-    else:
-        print "should remove : %s" % (c['nsid'])
-        to_remove_180.append(c['nsid'])
+for contact in [contact['nsid'] for contact in contacts]:
+    for delta in (180, 90, 60, 30):
+        if not has_photos(contact, deltas[delta]):
+            print "%s should remove : %s" % (delta, contact)
+            to_remove.setdefault(delta, []).append(contact)
+            break
 
 
-f = open(OPT.contact_to_remove_180, 'wb')
-pickle.dump(to_remove_180, f)
-f.close()
-
-to_remove_180.extend(to_remove_90)
-f = open(OPT.contact_to_remove_90, 'wb')
-pickle.dump(to_remove_180, f)
-f.close()
-
-to_remove_180.extend(to_remove_60)
-f = open(OPT.contact_to_remove_60, 'wb')
-pickle.dump(to_remove_180, f)
-f.close()
-
-to_remove_180.extend(to_remove_30)
-f = open(OPT.contact_to_remove_30, 'wb')
-pickle.dump(to_remove_180, f)
-f.close()
+for delta in (180, 90, 60, 30):
+    filename = getattr(OPT, 'contact_to_remove_%s' % delta)
+    f = open(filename, 'wb')
+    pickle.dump(to_remove[delta], f)
+    f.close()
