6807a608d88bc3a6ebafd553aad84790879de761
zaurky <zaurky@zeb.re>
2012-12-28 11:23:44 +0100

    do the flickr api init in API()

--- bin/getContact.py
+++ bin/getContact.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python
 
 from flickr_download_helper.api import API
-from flickr_download_helper.config import OptConfigReader, OPT, OptReader
-from flickr_download_helper.logger import Logger
-from flickr_download_helper.proxy import FDHProxySettings
-import os
+from flickr_download_helper.config import OPT
 
-config = OptConfigReader()
-config.setup()
-
-opt = OptReader()
-ret = opt.read('getContacts.py')
-
-
-## start the logger
-Logger().setup()
-Logger().warn("#############################################################")
-Logger().warn("Getting contacts (running as %s)"%(os.getpid()))
-
-proxy = FDHProxySettings()
-proxy.setValues(OPT)
-proxy.activate()
 
 # init of the flickr api
 api = API()
 
 
 def encode(string):
-    if not isinstance(string, (str, unicode)): return str(string)
-    try: return string.encode('latin1')
-    except: return string.encode('utf8')
+    if not isinstance(string, (str, unicode)):
+        return str(string)
+    try:
+        return string.encode('latin1')
+    except:
+        return string.encode('utf8')
 
-contact_id = OPT.user_id
+
 line = []
 
-c = api.getUserFromID(contact_id, True)
+c = api.getUserFromID(OPT.user_id, True)
 
 if OPT.getContactFields:
     for field in OPT.getContactFields:
         if field in c:
             line.append(c[field].strip())
+
 
 print "\t".join(map(encode, line))
