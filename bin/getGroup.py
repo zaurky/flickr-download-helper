c25b993c8f9c12401e3571aa464aa9579725c77e
zaurky <zaurky@zeb.re>
2012-12-28 14:10:07 +0100

    if API is not init, OPT dont exists

--- bin/getGroup.py
+++ bin/getGroup.py
@@ -4,5 +4,7 @@ from flickr_download_helper.api import API
 from flickr_download_helper.config import OPT
 
 
+api = API(False)
+
 if OPT.group_id:
-    API(False).groupFromScratch(OPT.group_id)
+    api.groupFromScratch(OPT.group_id)
