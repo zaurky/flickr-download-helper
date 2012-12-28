21c5e1863467750d7f8d11d393b0a7d3347e287f
zaurky <zaurky@zeb.re>
2012-12-28 13:46:44 +0100

    cleaner help message

--- flickr_download_helper/ipython_init.py
+++ flickr_download_helper/ipython_init.py
@@ -4,13 +4,22 @@ from flickr_download_helper.logger import Logger
 from flickr_download_helper.api import *
 import flickr_download_helper.api
 
+
+api = API(False)
+
+
+print "#" * 80 + "\n"
 print "\t" + "\n\t".join(filter(lambda func:
     'get' in func or 'search' in func,
     dir(flickr_download_helper.api)
 ))
 
+print "\t" + "\n\tapi.".join(filter(lambda func:
+    func.startswith('get') or func.startswith('search'),
+    dir(api)
+))
 
-print "#"*80
+print "\n" + "#" * 80
 print """
 contact_name = ''
 contact = api.getUserFromAll(contact_name)
@@ -22,8 +31,7 @@ photo = photos[0]
 info = api.getPhotoInfo(photo['id'])
 size = api.getPhotoSize(photo['id'])
 """
-print "#"*80
+print "\n" + "#" * 80
 
-api = API(False)
 
 me = OPT.my_id
