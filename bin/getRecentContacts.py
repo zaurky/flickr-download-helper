b511a5daacc58a0c5d99aab5ed8bf65cfb84b7f9
zaurky <zaurky@zeb.re>
2012-12-28 11:18:07 +0100

    do the flickr api init in API(False)

--- bin/getRecentContacts.py
+++ bin/getRecentContacts.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python
 
-from flickr_download_helper.api import initialisationFlickrApi
+from flickr_download_helper.api import API
 from flickr_download_helper import getRecentlyUploadedContacts
-from flickr_download_helper.config import OptConfigReader, OPT, OptReader
-from flickr_download_helper.logger import Logger
-from flickr_download_helper.proxy import FDHProxySettings
-import os
-
-config = OptConfigReader()
-config.setup()
-
-opt = OptReader()
-ret = opt.read('getContacts.py')
 
 
-## start the logger
-Logger().setup()
-Logger().warn("#############################################################")
-Logger().warn("Getting contacts (running as %s)"%(os.getpid()))
+# initi flickr api
+api = API(False)
 
-proxy = FDHProxySettings()
-proxy.setValues(OPT)
-proxy.activate()
-
-owners = getRecentlyUploadedContacts()
-
-print "\n".join(owners)
-
+print "\n".join(getRecentlyUploadedContacts())
