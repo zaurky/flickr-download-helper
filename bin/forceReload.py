c14fa5f1b8cd3062d47dd30b231f34d4e3f865a6
zaurky <zaurky@zeb.re>
2012-12-28 14:08:49 +0100

    do the flickr api init in API()

--- bin/forceReload.py
+++ bin/forceReload.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python
 
 from flickr_download_helper import Existing
-from flickr_download_helper.config import OptConfigReader, OPT
-from flickr_download_helper.proxy import FDHProxySettings
-from flickr_download_helper.logger import Logger
 from flickr_download_helper.api import API
 
-config = OptConfigReader()
-config.setup()
-
-Logger().setup()
-Logger().warn("#############################################################")
-Logger().warn("Reloading user photo cache")
-
-proxy = FDHProxySettings()
-proxy.setValues(OPT)
-proxy.activate()
 
 api = API()
 
